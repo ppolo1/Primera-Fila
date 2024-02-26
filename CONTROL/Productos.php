276869df88d46666edd6da29ad7c377ad58f029d
Adriath <wass@wass-Latitude-E7240>
2024-02-26 10:52:17 +0100

    Creada clase Productos

---
+++ CONTROL/Productos.php
@@ -0,0 +1,81 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+/**
+ * Description of Productos
+ *
+ * @author Primera Fila
+ */
+class Productos {
+    
+    private $idProducto ;
+    private $nombre ;
+    private $descripcion ;
+    private $precio ;
+    private $imagen ;
+    
+    public function __construct($nombre, $descripcion, $precio, $imagen, $idProducto = null) 
+    {
+        $this->idProducto = $idProducto;
+        $this->nombre = $nombre;
+        $this->descripcion = $descripcion;
+        $this->precio = $precio;
+        $this->imagen = $imagen;
+    }
+
+    
+    public function getIdProducto() {
+        return $this->idProducto;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getDescripcion() {
+        return $this->descripcion;
+    }
+
+    public function getPrecio() {
+        return $this->precio;
+    }
+
+    public function getImagen() {
+        return $this->imagen;
+    }
+
+    public function setIdProducto($idProducto): void {
+        $this->idProducto = $idProducto;
+    }
+
+    public function setNombre($nombre): void {
+        $this->nombre = $nombre;
+    }
+
+    public function setDescripcion($descripcion): void {
+        $this->descripcion = $descripcion;
+    }
+
+    public function setPrecio($precio): void {
+        $this->precio = $precio;
+    }
+
+    public function setImagen($imagen): void {
+        $this->imagen = $imagen;
+    }
+
+
+    public function jsonSerialize(){
+   
+        return [
+            'Nombre' => $this->nombre,
+            'Descripcion' => $this->descripcion,
+            'Precio' => $this->precio,
+            'Imagen' => $this->imagen,
+            'Id' => $this-> idProducto
+        ];        
+    
+    }
+}
--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once "./VISTA/Web.php" ;
 require_once './MODELO/BBDD.php';
 require_once './MODELO/Modelo.php';
 require_once './CONTROL/Controlador.php' ;
+require_once './CONTROL/Productos.php' ;
 
 //Web::completa() ;
 
