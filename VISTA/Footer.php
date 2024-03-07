0eafd6d6525fe001313f449c9340e8bae5d3241c
Adriath <adrianarjonabravo@gmail.com>
2024-03-07 20:54:17 +0100

    Cambios en el footer (se han añadido ids).

--- VISTA/Footer.php
+++ VISTA/Footer.php
@@ -1,31 +1,31 @@
 
 <footer class="container-fluid mt-5 bg-light">
-        <div class="row align-items-center text-center">
-            <div class="col-4 col-md-4">
-              <ul class="list-unstyled">
+    <div class="row align-items-center text-center">
+        <div class="col-4 col-md-4">
+            <ul class="list-unstyled">
                 <h5>Legalidad</h5>
-                <li><a href="politicaPrivacidad.html" class="text-decoration-none text-dark">Política de Privacidad</a></li>
-                <li><a href="cookies.html" class="text-decoration-none text-dark">Cookies</a></li>
-                <li><a href="avisolegal.html" class="text-decoration-none text-dark">Aviso legal</a></li>
+                <li><a id="privacidad" href="politicaPrivacidad.html" class="text-decoration-none text-dark">Política de Privacidad</a></li>
+                <li><a id="cookies" href="cookies.html" class="text-decoration-none text-dark">Cookies</a></li>
+                <li><a id="avisoLegal" href="avisolegal.html" class="text-decoration-none text-dark">Aviso legal</a></li>
             </ul>
-            </div>
-          <div class="col-4 col-md-4">
+        </div>
+        <div class="col-4 col-md-4">
             <h5>Redes Sociales</h5>
             <div class="social-icons">
-              <a href="#" class="fab fa-instagram"></a>
-              <a href="#" class="fab fa-facebook"></a>
-              <a href="#" class="fab fa-twitter"></a>                
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-facebook"></a>
+                <a href="#" class="fab fa-twitter"></a>                
             </div>
             <img class="imagen-producto mt-2" src="img/images.png" alt="Producto 2">
-          </div>
-          <div class="col-4">
+        </div>
+        <div class="col-4">
             <h5>Tienda</h5>
             <ul class="list-unstyled">
-              <li><a href="mueblesTienda.html" class="text-decoration-none text-dark">Muebles</a></li>
-              <li><a href="decoracionTienda.html" class="text-decoration-none text-dark">Decoración</a></li>
-              <li><a href="arteTienda.html" class="text-decoration-none text-dark">Arte</a></li>
-              <li><a href="iluminación.html" class="text-decoration-none text-dark">Iluminación</a></li>
+                <li><a href="mueblesTienda.html" class="text-decoration-none text-dark">Muebles</a></li>
+                <li><a href="decoracionTienda.html" class="text-decoration-none text-dark">Decoración</a></li>
+                <li><a href="arteTienda.html" class="text-decoration-none text-dark">Arte</a></li>
+                <li><a href="iluminación.html" class="text-decoration-none text-dark">Iluminación</a></li>
             </ul>
         </div>
     </div>
-      </footer>
+</footer>
--- VISTA/FooterContacto.php
+++ VISTA/FooterContacto.php
@@ -1,20 +1,25 @@
 
 <footer class="container-fluid mt-5 bg-light">
     <div class="row align-items-center text-center">
-        <div class="row align-items-center text-center">
-            <div class="col-4 col-md-4">
-                <h5>Política de Privacidad</h5>
-                <p>Información sobre Privacidad en nuestro sitio.</p>
-                <div class="privacidad">
-                    <a href="politicaPrivacidad.html">Más información</a>
-                </div>
-            </div>
-            <div class="col-4 col-md-4">
-                <img class="imagen-producto mt-2" src="img/images.png" alt="Producto 2">
-            </div>
-            <div class="col-4 col-md-4">
-                <h5>Derechos de Autor</h5>
-                <p>&copy; 2023 Eon Lux Antiques. Todos los derechos reservados.</p>
-            </div>
+        <div class="col-4 col-md-4">
+            <ul class="list-unstyled">
+                <h5>Legalidad</h5>
+                <li><a id="privacidad" href="politicaPrivacidad.html" class="text-decoration-none text-dark">Política de Privacidad</a></li>
+                <li><a id="cookies" href="cookies.html" class="text-decoration-none text-dark">Cookies</a></li>
+                <li><a id="avisoLegal" href="avisolegal.html" class="text-decoration-none text-dark">Aviso legal</a></li>
+            </ul>
         </div>
+        <div class="col-4 col-md-4">
+            <img class="imagen-producto mt-2" src="img/images.png" alt="Producto 2">
+        </div>
+        <div class="col-4">
+            <h5>Tienda</h5>
+            <ul class="list-unstyled">
+                <li><a href="mueblesTienda.html" class="text-decoration-none text-dark">Muebles</a></li>
+                <li><a href="decoracionTienda.html" class="text-decoration-none text-dark">Decoración</a></li>
+                <li><a href="arteTienda.html" class="text-decoration-none text-dark">Arte</a></li>
+                <li><a href="iluminación.html" class="text-decoration-none text-dark">Iluminación</a></li>
+            </ul>
+        </div>
+    </div>
 </footer>
