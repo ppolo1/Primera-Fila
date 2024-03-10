de300e13af4371f57aed2fa9c0b1a5175d8e67bd
Adriath <adrianarjonabravo@gmail.com>
2024-03-10 19:17:08 +0100

    Enrrtamiento funciona en un solo sentido. Sigue cambiarlo

--- VISTA/Footer.php
+++ VISTA/Footer.php
@@ -36,6 +36,8 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
 <script src="VISTA/js/javaScript.js"></script>
+<!--<script src="../VISTA/js/javaScript.js"></script>-->
 <script src="VISTA/js/jsRegistro.js"></script>
+<!--<script src="../VISTA/js/jsRegistro.js"></script>-->
 </body>
 </html>
--- VISTA/Header.php
+++ VISTA/Header.php
@@ -5,9 +5,12 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="VISTA/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="VISTA/css/style.css">
-        <link rel="stylesheet" href="VISTA/css/styleRegistro.css">
+        <!--<link href="VISTA/css/bootstrap.min.css" rel="stylesheet">-->
+        <link href="../VISTA/css/bootstrap.min.css" rel="stylesheet">
+        <!--<link rel="stylesheet" href="VISTA/css/style.css">-->
+        <link rel="stylesheet" href="../VISTA/css/style.css">
+        <!--<link rel="stylesheet" href="VISTA/css/styleRegistro.css">-->
+        <link rel="stylesheet" href="../VISTA/css/styleRegistro.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.0/css/bootstrap.min.css" rel="stylesheet">
@@ -25,7 +28,7 @@
                 </div>
                 <!-- Logo a la izquierda para todas las pantallas -->
                 <div class="col-3 col-md-3 col-lg-3 col-xl-3">
-                    <a href="home" class="navbar-brand">
+                    <a href="index.php/home" class="navbar-brand">
                         <img src="VISTA/img/logo_transparente2.png" alt="Logo" class="img-fluid">
                     </a>
                 </div>
