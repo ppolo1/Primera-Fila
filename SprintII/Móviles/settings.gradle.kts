ea80e3e88a3156634051efa28e393197b842a235
ppolo1 <84459476+ppolo1@users.noreply.github.com>
2024-03-12 13:00:42 +0100

    Update 1

---
+++ SprintII/Móviles/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Sprint2_Adrian_Adriana_DAW"
+include(":app")
